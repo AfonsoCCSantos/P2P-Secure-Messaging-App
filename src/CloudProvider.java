df790729edf9a56c3633ad7c0eb271a31466d25a
Afonso Santos <afonso.acc.santos@gmail.com>
2023-12-04 16:01:57 +0000

    created cloudprovider

---
+++ src/CloudProvider.java
@@ -0,0 +1,17 @@
+import java.util.Map;
+
+public class CloudProvider {
+	
+	private static Map<String, byte[]> data;
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	//Funcao para receber byte[] ou biginteger de um client e guardar
+	//no data, no mapa de cima
+	
+	//getter de byte[] ou biginteger do data (com segurança, por exemplo
+	//com a chave publica do cliente)
+}
